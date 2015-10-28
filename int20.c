d5661096a26a157b51e791d7eba110b6c614ef27
Nico01 <nicola.onorata@gmail.com>
2015-10-28 19:00:22 +0100

    fix int 20h

--- int20.c
+++ int20.c
@@ -3,13 +3,8 @@
 
 #include "int20.h"
 
-void int20(uc_engine *uc)
+void int20(uc_engine *uc) //terminate process
 {
-    uint16_t r_ip;
-    uint8_t r_ah;
-
-    uc_reg_read(uc, UC_X86_REG_AH, &r_ah);
-    uc_reg_read(uc, UC_X86_REG_IP, &r_ip);
-
-    printf(">>> 0x%x: interrupt: %x, AH = %02x\n", r_ip, 0x20, r_ah);
+    uc_emu_stop(uc);
+    //printf(">>> 0x%x: interrupt: %x, AH = %02x\n", r_ip, 0x20, r_ah);
 }
